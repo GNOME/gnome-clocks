bba035a492e1db75e862545c2477d57da0895e8d
Alex Anthony <alex.anthony28991@gmail.com>
2012-08-16 14:07:38 +0100

    Fix license info
    Needs a copying file adding ("You should have received a copy of the GNU
    General Public License along with Gnome Clocks").
    One file was missing a license block altogether.
    Each license block had a reference to Gnome Documents rather than
    Clocks.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=681950

--- gnomeclocks/clocks.py
+++ gnomeclocks/clocks.py
@@ -1,22 +1,20 @@
-"""
- Copyright (c) 2011-2012 Collabora, Ltd.
-
- Gnome Clocks is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by the
- Free Software Foundation; either version 2 of the License, or (at your
- option) any later version.
-
- Gnome Clocks is distributed in the hope that it will be useful, but
- WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- for more details.
-
- You should have received a copy of the GNU General Public License along
- with Gnome Documents; if not, write to the Free Software Foundation,
- Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-
- Author: Seif Lotfy <seif.lotfy@collabora.co.uk>
-"""
+# Copyright (c) 2011-2012 Collabora, Ltd.
+#
+# Gnome Clocks is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2 of the License, or (at your
+# option) any later version.
+#
+# Gnome Clocks is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with Gnome Clocks; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+# Author: Seif Lotfy <seif.lotfy@collabora.co.uk>
 
 from gi.repository import Gtk, GObject, Gio, Gdk, Gst, Notify, cairo
 from gi.repository.GdkPixbuf import Pixbuf
